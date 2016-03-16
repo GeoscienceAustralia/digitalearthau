1a00d563572b1f1bdf4cdd0488329e4a1f3dcbbe
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-03-16 16:15:56 +1100

    refactored package-module.sh to cover all variants and python versions
    see package-module.sh --help for more info

--- datacube-module/package-module.sh
+++ datacube-module/package-module.sh
@@ -4,35 +4,64 @@ set -eu
 
 umask 002
 
+variant=dev
+export module_dir=/g/data/v10/public/modules
+export agdc_env_module=agdc-py2-env/anaconda2-2.5.0
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+    --help)
+        echo Usage: $0 --variant ${variant} --env ${agdc_env_module} -- moduledir ${module_dir}
+        exit 0
+        ;;
+    --variant)
+        variant="$2"
+        shift # past argument
+        ;;
+    --env)
+        export agdc_env_module="$2"
+        shift # past argument
+        ;;
+    --moduledir)
+        export module_dir="$2"
+        shift # past argument
+        ;;
+    *)
+     # unknown option
+    ;;
+    esac
+shift # past argument or value
+done
+
+module load ${agdc_env_module}
+python_version=`python -c 'import sys; print "%s.%s"%sys.version_info[:2]'`
+python_major=`python -c 'import sys; print sys.version_info[0]'`
+subvariant=py${python_major}
+
 rm -rf agdc-v2 > /dev/null 2>&1
 git clone -b develop https://github.com/data-cube/agdc-v2.git
+pushd agdc-v2
 
-repository_location="agdc-v2"
-
-export py2_env_version=20160223
-
-module load agdc-py2-env/${py2_env_version}
-python --version
-
-pushd ${repository_location}
-
-# We export vars for envsubst below.
-export module_dir=/g/data/v10/public/modules
-export module_path=${module_dir}/modulefiles
+# TODO: what does this do? seems important
 module_requires=`python setup.py --requires`
 export version=`python setup.py --version`
-export package_name='agdc-py2-demo'
+
+export package_name=agdc-${subvariant}-${variant}
 export package_description=`python setup.py --description`
 
-export package_dest="${module_dir}/${package_name}/${version}"
-export python_dest="${package_dest}/lib/python2.7/site-packages"
+export package_dest=${module_dir}/${package_name}/${version}
+export python_dest=${package_dest}/lib/python${python_version}/site-packages
 
-echo "# Packaging '$package_name' v '$version' to '$package_dest' #"
+echo '# Packaging '$package_name' v '$version' to '$package_dest' #'
 
 read -p "Continue? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+
     # Setuptools requires the destination to be on the path, as it tests that the target is loadable.
     echo "Creating directory"
     mkdir -v -p "${python_dest}"
@@ -46,7 +75,7 @@ then
 
     popd
 
-    cp -v datacube-demo.conf ${package_dest}/datacube.conf
+    cp -v datacube-${variant}.conf ${package_dest}/datacube.conf
 
     modulefile_dest="${modulefile_dir}/${version}"
     envsubst < modulefile.template > "${modulefile_dest}"
@@ -57,4 +86,3 @@ rm -rf agdc-v2 > /dev/null 2>&1
 
 echo
 echo 'Done.'
-
