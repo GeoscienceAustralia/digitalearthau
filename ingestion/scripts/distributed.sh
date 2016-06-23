44809cd019b8b71e253b458d0dfa27866c51671c
v0lat1le <gregory.raevski@ga.gov.au>
2016-06-24 09:26:10 +1000

    updated distributed command names

--- ingestion/scripts/distributed.sh
+++ ingestion/scripts/distributed.sh
@@ -50,12 +50,12 @@ SCHEDULER_PORT=`shuf -i 2000-65000 -n 1`
 SCHEDULER_ADDR=$SCHEDULER_NODE:$SCHEDULER_PORT
 
 pbsdsh -n 0 -- /bin/bash -c "${init_env};\
-dscheduler --port $SCHEDULER_PORT"&
+dask-scheduler --port $SCHEDULER_PORT"&
 sleep 5s
 
 for ((i=0; i<PBS_NCPUS; i+=NCPUS)); do
   pbsdsh -n $i -- /bin/bash -c "${init_env};\
-dworker $SCHEDULER_ADDR --nprocs ${ppn} --nthreads ${tpp}"&
+dask-worker $SCHEDULER_ADDR --nprocs ${ppn} --nthreads ${tpp}"&
 done
 sleep 5s
 
