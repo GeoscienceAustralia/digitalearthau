c398d63e96a766d0d3d8cbefb61c91418bc30a45
v0lat1le <gregory.raevski@ga.gov.au>
2016-07-05 15:35:23 +1000

    improve cluster startup
    add delay between kicking workers off
    run fewer workers on the 'main' node
    use new command names

--- ingestion/scripts/distributed.sh
+++ ingestion/scripts/distributed.sh
@@ -36,9 +36,7 @@ do
 shift
 done
 
-init_env="umask ${umask};\
-source /etc/bashrc;\
-source ${env_script}"
+init_env="umask ${umask}; source /etc/bashrc; source ${env_script}"
 
 echo "*** ENVIRONMENT ***"
 cat ${env_script}
@@ -49,13 +47,18 @@ SCHEDULER_NODE=`sed '1q;d' $PBS_NODEFILE`
 SCHEDULER_PORT=`shuf -i 2000-65000 -n 1`
 SCHEDULER_ADDR=$SCHEDULER_NODE:$SCHEDULER_PORT
 
-pbsdsh -n 0 -- /bin/bash -c "${init_env};\
-dscheduler --port $SCHEDULER_PORT"&
+n0ppn=$(( $ppn < $NCPUS-2 ? $ppn : $NCPUS-2 ))
+n0ppn=$(( $n0ppn > 0 ? $n0ppn : 1 ))
+
+pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-scheduler --port $SCHEDULER_PORT"&
 sleep 5s
 
-for ((i=0; i<PBS_NCPUS; i+=NCPUS)); do
-  pbsdsh -n $i -- /bin/bash -c "${init_env};\
-dworker $SCHEDULER_ADDR --nprocs ${ppn} --nthreads ${tpp}"&
+pbsdsh -n 0 -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --nprocs ${n0ppn} --nthreads ${tpp}"&
+sleep 0.5s
+
+for ((i=NCPUS; i<PBS_NCPUS; i+=NCPUS)); do
+  pbsdsh -n $i -- /bin/bash -c "${init_env}; dask-worker $SCHEDULER_ADDR --nprocs ${ppn} --nthreads ${tpp}"&
+  sleep 0.5s
 done
 sleep 5s
 
