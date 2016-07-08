a8d36ca13ff644e84c9a1f61367478ae379741d3
v0lat1le <gregory.raevski@ga.gov.au>
2016-07-08 12:19:42 +1000

    use conda to setup environment

--- environment-module/package-module.sh
+++ environment-module/package-module.sh
@@ -4,36 +4,46 @@ set -eu
 
 umask 002
 
-export python_version='2.7.5'
-# TODO: Datacube tests fail when using gdal 2.0.0
-export gdal_version='1.11.1-python'
-export netcdf_version='4.3.3.1'
-export hdf5_version='1.8.14'
+variant=py2
+conda_url=http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+export module_dir=/g/data/v10/public/modules
 
-module load setuptools
+while [[ $# > 0 ]]
+do
+    key="$1"
 
-# Our install Python & gdal must match the ones run.
-module load python/${python_version}
-module load netcdf/${netcdf_version}
-module load gdal/${gdal_version}
-module load hdf5/${hdf5_version}
+    case $key in
+    --help)
+        echo Usage: $0 --variant ${variant} --moduledir ${module_dir} --conda ${conda_url}
+        exit 0
+        ;;
+    --variant)
+        variant="$2"
+        shift
+        ;;
+    --conda)
+        conda_url="$2"
+        shift # past argument
+        ;;
+    --moduledir)
+        export module_dir="$2"
+        shift # past argument
+        ;;
+    *)
+     # unknown option
+    ;;
+    esac
+shift # past argument or value
+done
 
-all_deps="$*"
-echo $all_deps
 
-export package_name=agdc-py2-env
+export package_name=agdc-${variant}-env
 
 # We export vars for envsubst below.
-export module_dir=/g/data/v10/public/modules
 export module_path=${module_dir}/modulefiles
 export version=$(date +'%Y%m%d')
 
 export package_dest="${module_dir}/${package_name}/${version}"
-export python_dest="${package_dest}/lib/python2.7/site-packages"
-
-
-# Compile deps with gcc, to match Python. (otherwise we get undefined intel symbols).
-export CC='gcc'
 
 echo "# Packaging '$package_name' '$version' to '$package_dest' #"
 
@@ -41,14 +51,10 @@ read -p "Continue? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Setuptools requires the destination to be on the path, as it tests that the target is loadable.
-    echo "Creating directory"
-    mkdir -v -p "${python_dest}"
-
-    PYTHONPATH="$PYTHONPATH:${python_dest}"
-    # The linux 64bit Dask tarball that pip austomatically installs is broken.
-    echo "Installing:"
-    easy_install "--prefix=${package_dest}" ${all_deps} dask-0.7.5.tar.gz matplotlib-1.5.1.tar.gz
+    wget ${conda_url} -O miniconda.sh
+    bash miniconda.sh -b -p ${package_dest}
+    ${package_dest}/bin/conda config --add channels conda-forge
+    ${package_dest}/bin/conda env create --file environment.yaml
 
     modulefile_dir="${module_dir}/modulefiles/${package_name}"
     mkdir -v -p "${modulefile_dir}"
