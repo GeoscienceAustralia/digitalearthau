46a3620efc09f2a119677f9e78bfdb830db90fe9
v0lat1le <gregory.raevski@ga.gov.au>
2016-07-08 10:11:51 +1000

    cleanup old stuff

--- environment-module/make-dev-package.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Checking dependencies..."
-dependencies=`./get-dependencies.sh`
-./package-module.sh setuptools pip pytest pytest-runner pytest-cov hypothesis pylint virtualenv ipython==4.0.0 jupyter ${dependencies}
-
