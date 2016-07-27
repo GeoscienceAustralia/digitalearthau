10849c5c892638a31eb729bfda6cc83df9d72b0a
v0lat1le <gregory.raevski@ga.gov.au>
2016-07-27 11:26:35 +1000

    add automagic database module packaging

--- database-module/datacube-ensure-user.sh
+++ database-module/datacube-ensure-user.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-db_server=130.56.244.227
+db_server=$1
+db_port=$2
 db_user=$USER
 
-if psql -h $db_server -U $db_user -w -c "select 1;" postgres > /dev/null ; then
+if psql -h $db_server -p $db_port -U $db_user -w -c "select 1;" postgres > /dev/null ; then
   exit 0
 fi
 
@@ -11,14 +12,13 @@ generated_key="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)"
 real_name="$(getent passwd $db_user | cut -d: -f5)"
 
 #Create user
-if ! psql -h $db_server -U guest -w -c "select create_readonly_agdc_user('$db_user', '$generated_key', '$real_name');" > /dev/null ; then
+if ! psql -h $db_server -p $db_port -U guest -w -c "select create_readonly_agdc_user('$db_user', '$generated_key', '$real_name');" > /dev/null ; then
   exit 1
 fi
 
 # Write pgpass
 (
     umask 077;
-    echo "$db_server:5432:*:$db_user:$generated_key" >> ~/.pgpass
-    echo "$db_server:6432:*:$db_user:$generated_key" >> ~/.pgpass
+    echo "$db_server:$db_port:*:$db_user:$generated_key" >> ~/.pgpass
 )
 
---
+++ database-module/package-module.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -eu
+
+umask 002
+
+export module_dir=/g/data/v10/public/modules
+export agdc_env_module=agdc-py2-env/anaconda2-2.5.0
+export pyavriant=py2
+export variant=prod
+export dbhost=130.56.244.227
+export dbport=6432
+export dbname=datacube
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+    --help)
+        echo Usage: $0 --moduledir ${module_dir} --name ${variant} --version "<version>" --dbname $dbname --dbhost $dbhost --dbport $dbport
+        exit 0
+        ;;
+    --moduledir)
+        export module_dir="$2"
+        shift # past argument
+        ;;
+   --name)
+        export variant="$2"
+        shift # past argument
+        ;;
+    --version)
+        export version="$2"
+        shift # past argument
+        ;;
+    *)
+     # unknown option
+    ;;
+    esac
+shift # past argument or value
+done
+
+export module_name=agdc-py2-${variant}
+export module_dest=${module_dir}/${module_name}/${version}
+export module_description="AGDC database configuration"
+
+echo '# Packaging '$module_name' v '$version' to '$module_dest' #'
+read -p "Continue? " -n 1 -r
+echo    # (optional) move to a new line
+
+function render {
+    envsubst < "$1" > "$2"
+    echo Wrote "$2"
+}
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 0
+fi
+
+mkdir -v -p "${module_dest}"
+cp -v datacube-ensure-user.sh "${module_dest}/"
+chmod +x "${module_dest}/datacube-ensure-user.sh"
+
+echo "[datacube]" > "${module_dest}/datacube.conf"
+echo db_hostname: ${dbhost} >> "${module_dest}/datacube.conf"
+echo db_port: ${dbport} >> "${module_dest}/datacube.conf"
+echo db_database: ${dbname} >> "${module_dest}/datacube.conf"
+
+modulefile_dir="${module_dir}/modulefiles/${module_name}"
+mkdir -v -p "${modulefile_dir}"
+
+render modulefile.template "${modulefile_dir}/${version}"
+
