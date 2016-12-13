c34a44e70a74bad7d8289a100a7254dd7518f286
Damien Ayers <damien@omad.net>
2016-12-13 11:33:47 +1100

    Print more helpful error messages when ensuring user exists

--- database-module/datacube-ensure-user.sh
+++ database-module/datacube-ensure-user.sh
@@ -8,22 +8,31 @@ db_server=$1
 db_port=$2
 db_user=$USER
 
-if psql -h $db_server -p $db_port -U $db_user -w -c "select 1;" postgres > /dev/null ; then
+if psql -h $db_server -p $db_port -U $db_user -w -c "select 1;" postgres > /dev/null 2>&1 ; then
   exit 0
 fi
 
-generated_key="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)"
-real_name="$(getent passwd $db_user | cut -d: -f5)"
+>&2 echo "Unable to connect to the database (${db_user}@${db_server}:${db_port})."
+>&2 echo
+>&2 echo "Attempting to create a new user for ${db_user}..."
+>&2 echo
 
->&2 echo "Unable to connect to the database (${db_server}:${db_port})."
->&2 echo "Attempting to create a new user for ${db_user}."
+generated_key=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+real_name=$(getent passwd $db_user | cut -d: -f5)
+
+user_already_exists_msg="An account for '$db_user' already exists in the Datacube Database, but we were unable to connect to it. This can happen if you have used the Datacube from raijin, and are now trying to access from VDI, or vice-versa. To fix this problem, please copy your ~/.pgpass file from the system you initially used to access the datacube, onto the current system."
 
 #Create user
 export PGPASSWORD=guest
-if ! psql -h $db_server -p $db_port -U guest -c "select create_readonly_agdc_user('$db_user', '$generated_key', '$real_name');" > /dev/null ; then
-  >&2 echo "Failed! Make sure your ~/.pgpass contains valid credentials."
+create_user_output=$((psql -h $db_server -p $db_port -U guest -c "select create_readonly_agdc_user('$db_user', '$generated_key', '$real_name');") 2>&1)
+if [[ $? != 0 && $create_user_output == *"already exists"* ]]; then
+  echo "$user_already_exists_msg" | fmt -t - 1>&2
   exit 1
+else
+  >&2 echo "Error creating user account for '$db_user'. "
+  >&2 echo ">> " $create_user_output
 fi
+
 
 # Write pgpass
 (
