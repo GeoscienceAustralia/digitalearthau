e3914d949241f7b73f859e53d0081ef4ca7f03ef
Jeremy Hooke <jez@stulk.com>
2017-01-20 16:09:43 +1100

    Add mocked index for testing

--- datacubenci/sync.py
+++ datacubenci/sync.py
@@ -1,9 +1,9 @@
-import logging
+import dawg
+import uuid
 from itertools import chain
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List, Mapping, Tuple
 
-import dawg
 import structlog
 from boltons import fileutils
 
@@ -12,37 +12,88 @@ from datacube.index import index_connect
 _LOG = structlog.get_logger()
 
 
-def make_path_set(log: logging.Logger,
-                  product: str,
-                  supplementary_paths: Iterable[Path]) -> dawg.CompletionDAWG:
-    with index_connect(application_name='sync') as index:
-        # assert db.in_transaction
-        log.info("paths.db.load")
-        all_db_results = index.datasets.search_returning(['uri'], product=product)
+class DatasetPathIndex:
+    def __init__(self):
+        super().__init__()
 
-        log.info("paths.trie")
-        uri_set = dawg.CompletionDAWG(
+    def iter_all_uris(self, product: str) -> Iterable[str]:
+        raise NotImplementedError
+
+    def get_dataset_ids_for_uri(self, uri: str) -> List[uuid.UUID]:
+        raise NotImplementedError
+
+    def add_location(self, dataset_id: uuid.UUID, uri: str) -> bool:
+        raise NotImplementedError
+
+    def remove_location(self, dataset_id: uuid.UUID, uri: str) -> bool:
+        raise NotImplementedError
+
+
+class AgdcDatasetPathIndex(DatasetPathIndex):
+    def __init__(self, index):
+        super().__init__()
+        self._index = index
+
+    def iter_all_uris(self, product: str) -> Iterable[str]:
+        for uri, in self._index.datasets.search_returning(['uri'], product=product):
+            yield str(uri)
+
+    @classmethod
+    def connect(cls) -> 'AgdcDatasetPathIndex':
+        return cls(index_connect(application_name='datacubenci-pathsync'))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        self._index.close()
+
+
+def iter_product_pathsets(product_locations: Mapping[str, Path],
+                          path_index: DatasetPathIndex,
+                          cache_path: Path) -> Iterable[Tuple[str, dawg.CompletionDAWG]]:
+    fileutils.mkdir_p(str(cache_path))
+    for product, filesystem_root in product_locations.items():
+        yield product, _build_pathset(filesystem_root, product, path_index)
+
+
+def _build_pathset(path_search_root: Path,
+                   product: str,
+                   path_index: DatasetPathIndex,
+                   cache_path: Path = None) -> dawg.CompletionDAWG:
+    log = _LOG.bind(product=product)
+
+    locations_cache = cache_path.joinpath(product + '-locations.dawg') if cache_path else None
+    if locations_cache and locations_cache.exists():
+        path_set = dawg.CompletionDAWG()
+        log.debug("paths.trie.cache.load", file=locations_cache)
+        path_set.load(str(locations_cache))
+    else:
+        log.info("paths.trie.build")
+        path_set = dawg.CompletionDAWG(
             chain(
-                (str(uri) for uri, in all_db_results),
-                (path.absolute().as_uri() for path in supplementary_paths)
+                path_index.iter_all_uris(product),
+                (path.absolute().as_uri() for path in path_search_root.rglob("ga-metadata.yaml"))
             )
         )
         log.info("paths.trie.done")
-    return uri_set
-
-
-def iter_product_pathsets(product_locations, cache_path):
-    fileutils.mkdir_p(str(cache_path))
-    for product, filesystem_root in product_locations.items():
-        log = _LOG.bind(product=product)
-        locations_cache = cache_path.joinpath(product + '-locations.dawg')
-
-        fileutils.atomic_save(str(locations_cache))
-        if locations_cache.exists():
-            path_set = dawg.CompletionDAWG()
-            path_set.load(str(locations_cache))
-        else:
-            path_set = make_path_set(log, product, filesystem_root.rglob("ga-metadata.yaml"))
+        if locations_cache is not None:
+            log.debug("paths.trie.cache.create", file=locations_cache)
             with fileutils.atomic_save(str(locations_cache)) as f:
                 path_set.write(f)
-        yield product, path_set
+    return path_set
+
+
+def main():
+    root = Path('/tmp/test')
+    cache = root.joinpath('cache')
+    products = {
+        'ls8_level1_scene': root.joinpath('ls8_scenes'),
+        'ls7_level1_scene': root.joinpath('ls7_scenes'),
+    }
+
+    with AgdcDatasetPathIndex.connect() as path_index:
+        iter_product_pathsets(products, path_index, cache_path=cache)
+
+if __name__ == '__main__':
+    main()
--- datacubenci/test_sync.py
+++ datacubenci/test_sync.py
@@ -1,5 +1,41 @@
+import uuid
+from typing import Iterable, List
+
+from boltons.dictutils import MultiDict
 from datacubenci import sync
 from datacubenci.paths import write_files
+
+
+# In-memory index, so that we can test without using a real datacube index.
+class MemoryDatasetPathIndex(sync.DatasetPathIndex):
+    def __init__(self):
+        super().__init__()
+        # Map paths to lists of dataset ids.
+        self._records = MultiDict()
+
+    def iter_all_uris(self, product: str) -> Iterable[str]:
+        return self._records.keys()
+
+    def add_location(self, dataset_id: uuid.UUID, uri: str) -> bool:
+        ids = self._records.getlist(uri)
+        if dataset_id in ids:
+            # Not added
+            return False
+
+        self._records.add(uri, dataset_id)
+        return True
+
+    def remove_location(self, dataset_id: uuid.UUID, uri: str) -> bool:
+        if dataset_id not in self.get_dataset_ids_for_uri(uri):
+            # Not removed
+            return False
+
+        ids = self._records.popall(uri)
+        ids.remove(dataset_id)
+        self._records.addlist(uri, ids)
+
+    def get_dataset_ids_for_uri(self, uri: str) -> List[uuid.UUID]:
+        return list(self._records.getlist(uri))
 
 
 def test_iter_product_pathsets():
@@ -22,29 +58,48 @@ def test_iter_product_pathsets():
         }
     )
 
+    index = MemoryDatasetPathIndex()
+    already_indexed_uri = root.joinpath('indexed', 'already', 'ga-metadata.yaml').as_uri()
+    index.add_location(uuid.UUID('1e47df58-de0f-11e6-93a4-185e0f80a5c0'),
+                       already_indexed_uri)
+
     products = {
         'ls8_level1_scene': root.joinpath('ls8_scenes'),
         'ls7_level1_scene': root.joinpath('ls7_scenes'),
     }
 
     expected_paths = {
-        'ls7_level1_scene': [],
-        'ls8_level1_scene': [root.joinpath('ls8_scenes', 'ls8_test_dataset', 'ga-metadata.yaml')]
+        'ls7_level1_scene': [
+            already_indexed_uri
+        ],
+        'ls8_level1_scene': [
+            root.joinpath('ls8_scenes', 'ls8_test_dataset', 'ga-metadata.yaml').as_uri(),
+            already_indexed_uri
+        ]
     }
 
     cache_path = root.joinpath('cache')
+
+    expected_path_count = {
+        'ls7_level1_scene': 1,
+        'ls8_level1_scene': 2
+    }
+
+    # It should find and add all of the on-disk datasets
     product_return_count = 0
-    for product, path_set in sync.iter_product_pathsets(products, cache_path):
+    for product, path_set in sync.iter_product_pathsets(products, index, cache_path):
         product_return_count += 1
         expected = expected_paths.pop(product, None)
         assert expected is not None, "Product {} not expected (again?)".format(product)
 
         # All the paths we expect should be there.
         for expected_path in expected:
-            assert expected_path.absolute().as_uri() in path_set
+            assert expected_path in path_set
 
         # A our dummy outside of the product folder should not
         dummy_dataset = root.joinpath('dummy_dataset', 'ga-metadata.yaml')
         assert dummy_dataset.absolute().as_uri() not in path_set
+
+        assert sum(1 for p in path_set.iterkeys('file://')) == expected_path_count[product]
 
     assert product_return_count == len(products)
