5513413ca138abc9e942ed16b72701d99ee6adaa
Jeremy Hooke <jez@stulk.com>
2017-01-23 17:52:03 +1100

    Use pep8 instead of pylint for pep8 check
    pylint doesn't handle whitespace around optional types properly.
    pep8 tool accepts them the way PyCharm formats them.

--- check-code.sh
+++ check-code.sh
@@ -7,6 +7,8 @@ set -x
 py_files=$(find scripts -name '*.py')
 
 pylint -j 2 --reports no datacubenci ${py_files}
+# 'continuation line' has too many spurious errors.
+pep8 --ignore=E122 --max-line-length 120 datacubenci ${py_files}
 
 # Users can specify extra folders as arguments.
 py.test -r sx --durations=5 datacubenci ${py_files} $@
