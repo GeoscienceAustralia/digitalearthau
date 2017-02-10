b032fd7c86c93a3674597c6af7c101ea8a9b70ff
Jeremy Hooke <jez@stulk.com>
2017-02-10 14:37:51 +1100

    Make python env version a parameter

--- modules/agdc-instances/package-all-instances.sh
+++ modules/agdc-instances/package-all-instances.sh
@@ -2,20 +2,24 @@
 
 echoerr() { echo "$@" 1>&2; }
 
-if [[ $# != 1 ]] || [[ "$1" == "--help" ]];
+if [[ $# != 2 ]] || [[ "$1" == "--help" ]];
 then
     echoerr
-    echoerr "Usage: $0 <version>"
+    echoerr "Usage: $0 <py-module-version> <datacube-version>"
+    echoerr"        eg. $0 20161201 1.1.17"
     exit 1
 fi
-export version="$1"
+export py_module_version="$1"
+export version="$2"
 
-export agdc_env_module=agdc-py3-env/20161201
+export agdc_env_module=agdc-py3-env/${py_module_version}
 
 pyvariant=py3 variant=dev dbhost=130.56.244.225 ./package-instance-module.sh $version
 pyvariant=py3 variant=futureprod dbhost=130.56.244.105 ./package-instance-module.sh $version
 pyvariant=py3 variant=prod dbhost=130.56.244.227 ./package-instance-module.sh $version
 
-export agdc_env_module=agdc-py2-env/20161201
+
+export agdc_env_module=agdc-py2-env/${py_module_version}
+
 pyvariant=py2 variant=prod dbhost=130.56.244.227 ./package-instance-module.sh $version
 
