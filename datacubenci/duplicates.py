fc1b6474e695a829a9f021c24250b247e5de06ec
Jeremy Hooke <jez@stulk.com>
2017-02-14 17:40:47 +1100

    Add scene duplicate finder

---
+++ datacubenci/duplicates.py
@@ -0,0 +1,128 @@
+# coding=utf-8
+"""
+Find duplicate datasets
+(eg. if a dataset has been reprocessed but both versions are indexed and active)
+
+"""
+import csv
+import sys
+from datetime import datetime
+from typing import Iterable
+from uuid import UUID
+
+from dateutil import tz
+from psycopg2._range import Range
+from singledispatch import singledispatch
+
+from datacube.index import index_connect
+from datacube.index._api import Index
+from datacube.index.fields import Field
+from datacube.model import DatasetType
+
+
+def main():
+    with index_connect(application_name='find-duplicates') as c:
+        # Currently we're only looking at scenes, using path/row/time.
+
+        scene_products = [p for p in c.products.get_all() if p.name.endswith('scene')]
+
+        has_started = False
+        for product in scene_products:
+            field = product.metadata_type.dataset_fields.get
+            unique_fields = (
+                field('sat_path').lower,
+                field('sat_row').lower,
+                field('time').lower.day,
+            )
+
+            _write_csv(
+                unique_fields,
+                get_dupes(c, unique_fields, product),
+                append=has_started
+            )
+            has_started = True
+
+
+def get_dupes(index, unique_fields, product):
+    # type: (Index, Iterable[Field], DatasetType) -> Iterable[dict]
+    headers = _get_headers(unique_fields)
+
+    for group, dataset_refs in index.datasets.search_product_duplicates(product, *unique_fields):
+        values = (product.name,) + group + (len(dataset_refs), list(dataset_refs))
+        yield dict(zip(headers, values))
+
+
+def _get_headers(unique_fields):
+    # type: (Iterable[Field]) -> Iterable[str]
+    return ('product',) + tuple(f.name for f in unique_fields) + ('count', 'dataset_refs',)
+
+
+@singledispatch
+def printable(val):
+    return val
+
+
+@printable.register(type(None))
+def printable_none(val):
+    return ''
+
+
+@printable.register(datetime)
+def printable_dt(val):
+    """
+    :type val: datetime.datetime
+    """
+    return _assume_utc(val).isoformat()
+
+
+def _assume_utc(val):
+    if val.tzinfo is None:
+        return val.replace(tzinfo=tz.tzutc())
+    else:
+        return val.astimezone(tz.tzutc())
+
+
+@printable.register(Range)
+def printable_r(val):
+    """
+    :type val: psycopg2._range.Range
+    """
+    if val.lower_inf:
+        return printable(val.upper)
+    if val.upper_inf:
+        return printable(val.lower)
+
+    return printable(val.lower)
+
+
+@printable.register(list)
+def printable_list(val):
+    """
+    Space separated
+
+    (space separated list of uuids is useful for copying to use as cli args.)
+
+    :type val: list
+    """
+    return ' '.join(printable(v) for v in val)
+
+
+@printable.register(UUID)
+def printable_uuid(val):
+    return str(val)
+
+
+def _write_csv(unique_fields, dicts, append=False):
+    writer = csv.DictWriter(sys.stdout, _get_headers(unique_fields))
+    if not append:
+        writer.writeheader()
+    writer.writerows(
+        (
+            {k: printable(v) for k, v in d.items()}
+            for d in dicts
+        )
+    )
+
+
+if __name__ == '__main__':
+    main()
