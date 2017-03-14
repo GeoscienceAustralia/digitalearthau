a15581d946355551b8b0d9d364bc0cc4fe538668
Jeremy Hooke <jez@stulk.com>
2017-03-14 11:27:09 +1100

    Add spacing to user-exists-error for readability

--- modules/agdc-instances/datacube-ensure-user.sh
+++ modules/agdc-instances/datacube-ensure-user.sh
@@ -20,14 +20,17 @@ fi
 generated_key=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
 real_name=$(getent passwd $db_user | cut -d: -f5)
 
-user_already_exists_msg="An account for '$db_user' already exists in the Datacube Database, but we were unable to connect to it. This can happen if you have used the Datacube from raijin, and are now trying to access from VDI, or vice-versa. To fix this problem, please copy your ~/.pgpass file from the system you initially used to access the datacube, onto the current system."
+user_already_exists_msg="An account for '$db_user' already exists, but we were unable to connect to it.\n
+ -> This can happen if you have used the Datacube from raijin, and are now trying to access from VDI, or vice-versa.\n
+ -> To fix this problem, please copy your ~/.pgpass file from the system you initially used to access the datacube, onto the current system.\n
+"
 
 #Create user
 export PGPASSWORD=guest
 create_user_output=$((psql -h $db_server -p $db_port -U guest guest -c "select create_readonly_agdc_user('$db_user', '$generated_key', '$real_name');") 2>&1)
 if [[ $? != 0 ]]; then
     if [[ $create_user_output == *"already exists"* ]]; then
-        echo "$user_already_exists_msg" | fmt -t - 1>&2
+        printf "$user_already_exists_msg" | fmt - 1>&2
     else
         >&2 echo "Error creating user account for '$db_user'. "
         >&2 echo ">> " $create_user_output
