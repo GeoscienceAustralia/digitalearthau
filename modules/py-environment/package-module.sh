307300930308bacd526dc63fb456cf5b9431365a
Jeremy Hooke <jez@stulk.com>
2017-04-27 11:21:41 +1000

    Set py env module root folder permissions

--- modules/py-environment/package-module.sh
+++ modules/py-environment/package-module.sh
@@ -67,6 +67,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
     wget ${conda_url} -O miniconda.sh
     bash miniconda.sh -b -p ${package_dest}
+
+    # The root folder (but not its contents) is missing public read/execute by default (?)
+    chmod a+rx ${package_dest}
+
     ${package_dest}/bin/conda config --prepend channels conda-forge --system
     # update root env to the latest python and packages
     ${package_dest}/bin/conda update --all -y
