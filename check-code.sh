855a4ef72a1805ab3c16812b0a0619094dee4168
Jeremy Hooke <jez@stulk.com>
2017-06-02 11:53:48 +1000

    Travis/test rename datacubenci→digitalearthau

--- check-code.sh
+++ check-code.sh
@@ -6,10 +6,10 @@ set -x
 
 py_files=$(find scripts -name '*.py')
 
-pylint -j 2 --reports no datacubenci ${py_files}
+pylint -j 2 --reports no digitalearthau ${py_files}
 # 'continuation line' has too many spurious errors.
-pep8 --ignore=E122 --max-line-length 120 datacubenci ${py_files}
+pep8 --ignore=E122 --max-line-length 120  ${py_files}
 
 # Users can specify extra folders as arguments.
-py.test -r sx --durations=5 datacubenci ${py_files} $@
+py.test -r sx --durations=5 digitalearthau ${py_files} $@
 
