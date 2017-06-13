0981de0313ff353dc07e9ae578c31a4f01a93298
Andrew Hicks <andrew.hicks@ga.gov.au>
2017-06-13 17:49:53 +1000

    Push compiled docs to gh-docs branch

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -29,10 +29,9 @@ SHA=`git rev-parse --verify HEAD`
 git clone $REPO _build/html
 cd _build/html
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ../..
-
 # Clean out existing contents
-rm -rf _build/html/**/* || exit 0
+rm -rf ./*
+cd ../..
 
 # Run our compile script
 doCompile
@@ -47,6 +46,24 @@ if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add -A .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../dea-docs-gen.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
 
 echo "deploy.sh done"
 exit 0
