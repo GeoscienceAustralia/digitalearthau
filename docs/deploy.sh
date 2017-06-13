f00351071bb2de366f6a86d54053a205a9dde6c4
Andrew Hicks <andrew.hicks@ga.gov.au>
2017-06-13 17:59:33 +1000

    Fix relative paths

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")" # cd into this directory
 
 SOURCE_BRANCH="wip-docs"  # Should be "master" when in production
 TARGET_BRANCH="gh-pages"
+BUILD_PATH="_build/html"
 COMMIT_AUTHOR_EMAIL=`git show --format="%aE" -s`
 ENCRYPTION_LABEL="c4bf5207aec3"
 
@@ -26,24 +27,25 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO _build/html
-cd _build/html
+git clone $REPO $BUILD_PATH
+pushd $BUILD_PATH
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 # Clean out existing contents
 rm -rf ./*
-cd ../..
+popd
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
-cd out
+pushd $BUILD_PATH
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
+    popd
     exit 0
 fi
 
@@ -51,19 +53,22 @@ fi
 # The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
+popd
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../dea-docs-gen.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in dea-docs-gen.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
+pushd $BUILD_DIR
 git push $SSH_REPO $TARGET_BRANCH
+popd
 
 echo "deploy.sh done"
 exit 0
