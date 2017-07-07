dcdc5f306e57d6c4d36a095fdf510b22f811be80
Jeremy Hooke <jez@stulk.com>
2017-07-07 11:54:26 +1000

    Add initial integration_test framework
    Testing out existing config files.

--- check-code.sh
+++ check-code.sh
@@ -6,12 +6,21 @@ set -x
 
 shopt -s globstar
 
-pylint -j 2 --reports no digitalearthau scripts/**/*.py
+pylint -j 2 --reports no \
+    digitalearthau \
+    scripts/**/*.py
+
+# We check the integration tests even though they aren't run by default here.
+pylint --rcfile=integration_tests/pylintrc integration_tests/**/*.py
+
 # 'continuation line' has too many spurious errors.
-pep8 --ignore=E122 --max-line-length 120  digitalearthau scripts/**/*.py
+pep8 --ignore=E122 --max-line-length 120  \
+    digitalearthau \
+    integration_tests \
+    scripts/**/*.py
 
 shellcheck ./**/*.sh 
 
-# Users can specify extra folders as arguments.
+# Users can specify extra folders (ie. integration_tests) as arguments.
 py.test -r sx --durations=5 digitalearthau scripts/**/*.py "$@"
 
---
+++ integration_tests/test_config.py
@@ -0,0 +1,49 @@
+from datacube.index._api import Index
+
+
+def test_dea_config(dea_index: Index):
+    """
+    Check that all the product definitions are valid enough to be added, and that they are added.
+
+    """
+    md_names = set(md.name for md in dea_index.metadata_types.get_all())
+    # Sanity check that it doesn't return duplicates
+    assert len(list(dea_index.metadata_types.get_all())) == len(md_names)
+
+    assert md_names == {
+        'eo',
+        'landsat_l1_scene',
+        'landsat_scene',
+        'telemetry',
+    }
+
+    products_names = set(product.name for product in dea_index.products.get_all())
+    # Sanity check that it doesn't return duplicates
+    assert len(list(dea_index.products.get_all())) == len(products_names)
+
+    # All expected products.
+
+    # There's a few subtle mistakes we've seen before, such as forgetting the yaml document separator
+    # between two products, causing the latter product fields to completely override the former.
+    assert products_names == {
+        'dsm1sv10',
+        'ls5_level1_scene',
+        'ls5_nbar_scene',
+        'ls5_nbart_scene',
+        'ls5_pq_scene',
+        'ls5_satellite_telemetry_data',
+        'ls7_level1_scene',
+        'ls7_nbar_scene',
+        'ls7_nbart_scene',
+        'ls7_pq_scene',
+        'ls7_satellite_telemetry_data',
+        'ls8_level1_oli_scene',
+        'ls8_level1_scene',
+        'ls8_nbar_oli_scene',
+        'ls8_nbar_scene',
+        'ls8_nbart_oli_scene',
+        'ls8_nbart_scene',
+        'ls8_pq_oli_scene',
+        'ls8_pq_scene',
+        'ls8_satellite_telemetry_data',
+    }
