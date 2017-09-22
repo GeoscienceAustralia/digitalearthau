3c5cca41b63b15867a9373725e0325d48618a0bd
Kirill Kouzoubov <kirill888@gmail.com>
2017-09-22 16:54:53 +1000

    Update pq count product yaml
    - uint16 -> int16
    - nodata -> -1
    - product_type  -> pq_count
    - Split annual vs all time product

--- integration_tests/test_config.py
+++ integration_tests/test_config.py
@@ -61,5 +61,6 @@ def test_dea_config(dea_index: Index):
         'ls8_pq_oli_scene',
         'ls8_pq_scene',
         'ls8_satellite_telemetry_data',
-        'pq_count_albers',
+        'pq_count_summary',
+        'pq_count_annual_summary',
     }
