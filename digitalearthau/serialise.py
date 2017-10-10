4d5803d342d8802227e66432942e1b89b00c322d
Jeremy Hooke <jez@stulk.com>
2017-10-10 17:32:45 +1100

    Add serial/deserial methods for tuple structures

--- digitalearthau/serialise.py
+++ digitalearthau/serialise.py
@@ -4,13 +4,17 @@ import os
 import socket
 import uuid
 import datetime
-from typing import NamedTuple, Optional, List
+
+from typing import NamedTuple, Optional, List, Dict, Union
 
 from enum import Enum, unique
 
 import pathlib
+import dateutil.parser
 
 import yaml
+
+from digitalearthau import paths
 
 
 class JsonLinesWriter:
@@ -44,7 +48,7 @@ def to_json(o, *args, **kwargs):
     """
     return json.dumps(
         o,
-        default=_json_fallback,
+        default=simplify_obj,
         separators=(', ', ':'),
         sort_keys=True,
     )
@@ -85,3 +89,100 @@ def dump_document(path: pathlib.Path, object, allow_unsafe=False):
         path.write_text(
             to_json(object)
         )
+
+
+def dump_structure(path: pathlib.Path, object):
+    """
+    Dump NamedTuples to a yaml/json document
+    """
+    return dump_document(path, named_tuple_to_dict(object))
+
+
+def load_structure(path: pathlib.Path, expected_type):
+    """
+    Load the expected NamedTuple (with type hints) from a yaml/json
+    """
+    return deserial(paths.read_document(path), expected_type)
+
+
+def simplify_obj(obj):
+    """Make common types serialisable as json/yaml/whatever."""
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+
+    if isinstance(obj, (pathlib.Path, uuid.UUID)):
+        return str(obj)
+
+    if isinstance(obj, set):
+        return list(obj)
+
+    try:
+        # Allow class to define their own.
+        return obj.to_dict()
+    except AttributeError:
+        return obj
+
+
+def named_tuple_to_dict(o):
+    """
+    Convert a named tuple and all of its directly-embedded named tuples to serialisable dicts
+    """
+    # TODO: Doesn't handle indirectly embedded NamedTuples, such as within a list.
+
+    # We can't isinstance() for NamedTuple, the noraml way is to see if attributes like _fields exist.
+    try:
+        return dict(zip(o._fields, (named_tuple_to_dict(value) for value in o)))
+    except AttributeError:
+        return simplify_obj(o)
+
+
+def deserial(o, expected_type):
+    """
+    Try to parse the given dict into an object tree.
+    """
+    if expected_type in (pathlib.Path, uuid.UUID):
+        return expected_type(o)
+
+    if expected_type == datetime.datetime:
+        return dateutil.parser.parse(o)
+
+    # We can't isinstance() for NamedTuple, the noraml way is to see if attributes like _fields exist.
+    try:
+        field_types: Dict = expected_type._field_types
+        assert isinstance(o, dict)
+        return expected_type(
+            **{k: deserial(v, field_types[k]) for (k, v) in o.items()}
+        )
+    except AttributeError:
+        pass
+    return o
+
+
+def test_serial():
+    # Convert to dict and back again, checking that it's identical.
+
+    class EmbeddedObj(NamedTuple):
+        a: str
+        my_dt: datetime.datetime
+
+    class ObjA(NamedTuple):
+        v1: str
+        v2: List[int]
+        mynamedtuple: EmbeddedObj
+        my_path: pathlib.Path
+
+    m = ObjA(
+        "a string",
+        [1, 2, 3],
+        EmbeddedObj(
+            "b string",
+            datetime.datetime.utcnow()
+        ),
+        pathlib.Path("/tmp/test")
+    )
+
+    d = named_tuple_to_dict(m)
+    print(repr(d))
+    new_m = deserial(d, ObjA)
+    print(repr(new_m))
+    assert m == new_m
