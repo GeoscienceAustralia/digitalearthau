e4e61d36339c93b4c7a2c97c88dd06c5aef0ba5c
Jeremy Hooke <jez@stulk.com>
2017-10-25 11:42:18 +1100

    Add a test of a basic dataset file move

--- integration_tests/test_move.py
+++ integration_tests/test_move.py
@@ -2,46 +2,32 @@ from pathlib import Path
 
 from click.testing import CliRunner, Result
 
-from digitalearthau import move, collections, paths
-from digitalearthau.collections import Collection
-from digitalearthau.index import add_dataset
-from integration_tests.conftest import DatasetOnDisk
-
-import pytest
-import structlog
-from dateutil import tz
+from digitalearthau import move, paths
+from integration_tests.conftest import DatasetForTests
 
 
-def test_move(global_integration_cli_args, test_dataset: DatasetOnDisk, other_dataset: DatasetOnDisk, tmpdir):
+def test_move(global_integration_cli_args,
+              test_dataset: DatasetForTests,
+              other_dataset: DatasetForTests,
+              tmpdir):
     """
-    With two datasets in a collection, try to move one.
+    With two datasets in a collection, try to move one to a new disk location.
+
+    One should be unchanged, the other should be moved.
     """
-
-    # Add second dataset
-    add_dataset(test_dataset.collection.index, test_dataset.dataset.id, test_dataset.uri)
-    add_dataset(other_dataset.collection.index, other_dataset.dataset.id, other_dataset.uri)
-
-    # Move one dataset.
-
-    runner = CliRunner()
+    test_dataset.add_to_index()
+    other_dataset.add_to_index()
 
     destination = Path(tmpdir).joinpath('destination_collection')
     destination.mkdir(exist_ok=False)
 
     paths.register_base_directory(destination)
 
-    # dest_collection = Collection(
-    #     name='ls8_scene_dest',
-    #     query={},
-    #
-    #     index=test_dataset.collection.index
-    # )
-    # collections._add(dest_collection)
+    res = _call_move(
+        # Move one path to destination
+        ['-d', str(destination), str(test_dataset.path)],
 
-    res: Result = runner.invoke(
-        move.cli,
-        [*global_integration_cli_args, '-d', str(destination), str(test_dataset.path)],
-        catch_exceptions=False
+        global_integration_cli_args
     )
 
     print(res.output)
@@ -50,20 +36,32 @@ def test_move(global_integration_cli_args, test_dataset: DatasetOnDisk, other_da
     assert destination.exists(), "File was not moved to destination?"
     assert test_dataset.path.exists(), "Old file was deleted: should only have been archived"
 
-    # Only the destination should be active now, so only it will be returned in search
-
-    all_indexed_uris = set(test_dataset.collection.iter_index_uris())
-    # The 'other' dataset should be unchanged.
+    # Expect a destination with the same offset (eg "year/LS8_SOMETHING_AROTHER") but in our new base folder.
     expected_new_path = destination.joinpath(*test_dataset.path_offset)
+    expected_new_uri = expected_new_path.as_uri()
+
+    # All three uris should be in the system still.
+    all_indexed_uris = set(test_dataset.collection.iter_index_uris())
     assert all_indexed_uris == {
         # The old uri one still indexed (but archived)
         test_dataset.uri,
         # The new uri
-        expected_new_path.as_uri(),
+        expected_new_uri,
         # The other dataset unchanged
         other_dataset.uri
     }
 
-    dataset = test_dataset.collection.index.get(test_dataset.dataset.id)
+    # ... but only the new uri is active, so the moved dataset should only list that one:
+    dataset = test_dataset.get_index_record()
+    assert dataset is not None, "Moved dataset is no longer indexed??"
+    assert dataset.uris == [expected_new_uri], "Expect only the newly moved location to be listed for the dataset"
 
-    test_dataset.collection.iter_index_uris()
+
+def _call_move(args, global_integration_cli_args) -> Result:
+    # We'll call the cli interface itself, rather than the python api, to get wider coverage in our test.
+    res: Result = CliRunner().invoke(
+        move.cli,
+        [*global_integration_cli_args, *args],
+        catch_exceptions=False
+    )
+    return res
