d188050dca98d618e928a6e81dc29728bc1d1573
Jeremy Hooke <jez@stulk.com>
2017-12-21 14:10:57 +1100

    Local sphinx builds don't have the 'logo_only' option

--- docs/conf.py
+++ docs/conf.py
@@ -104,16 +104,12 @@ try:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'logo_only': True,
+    }
 except ImportError:
     html_theme = 'alabaster'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {
-    'logo_only': True,
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
