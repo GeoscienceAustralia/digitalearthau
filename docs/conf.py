38500d868fd98569920cd10b061804b31ef75a38
Damien Ayers <damien@omad.net>
2018-02-16 10:00:04 +1100

    Sphinx disallows extra html_theme_options in unsupported themes

--- docs/conf.py
+++ docs/conf.py
@@ -104,13 +104,13 @@ try:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'logo_only': True,
+        'analytics_id': 'UA-113800428-1',
+    }
 except ImportError:
     html_theme = 'alabaster'
 
-html_theme_options = {
-    'logo_only': True,
-    'analytics_id': 'UA-113800428-1',
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
