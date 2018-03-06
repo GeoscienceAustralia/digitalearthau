ea934beae69272e46dcab8f98f2669ccc7844a7a
Damien Ayers <damien@omad.net>
2018-03-06 14:28:30 +1100

    WIP Simplifying the NCI module creation
    - Only two parallel modules, instead of 6? 7? too many
    - Get rid of references to AGDC

--- modules/py-environment/package-module.sh
+++ modules/py-environment/package-module.sh
@@ -4,8 +4,10 @@ set -eu
 
 umask 022
 
-variant=py2
 export module_dir=/g/data/v10/public/modules
+
+python='3.6'
+conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 while [[ $# -gt 0 ]]
 do
@@ -13,12 +15,8 @@ do
 
     case $key in
     --help)
-        echo "Usage: $0 --variant ${variant} --moduledir ${module_dir}"
+        echo "Usage: $0 [--conda CONDA_DOWNLOAD_URL] --moduledir ${module_dir}"
         exit 0
-        ;;
-    --variant)
-        variant="$2"
-        shift
         ;;
     --conda)
         conda_url="$2"
@@ -36,32 +34,19 @@ do
 shift # past argument or value
 done
 
-case $variant in
-py2)
-    python='2.7'
-    conda_url=https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-    ;;
-py3)
-    python='3.6'
-    conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    ;;
-*)
-    echo Unknown variant. Must be one of py2, py3
-    exit 1
-    ;;
-esac
-
 
 tmp_dir="$(mktemp -d)"
 
-package_name=agdc-${variant}-env
+package_name=dea-env
 module_path=${module_dir}/modulefiles
 version=${version:-$(date +'%Y%m%d')}
-package_description="Datacube environment module"
+package_description="DEA environment module"
 package_dest="${module_dir}/${package_name}/${version}"
 
 # We export vars for envsubst below.
 export package_name module_path version package_description package_dest
+
+# TODO, check that we haven't already created an environment module today
 
 echo "# Packaging '$package_name' '$version' to '$package_dest' #"
 
@@ -99,6 +84,7 @@ then
     modulefile_dir="${module_dir}/modulefiles/${package_name}"
     mkdir -v -p "${modulefile_dir}"
 
+    # Write the modulefile to enable people to load
     modulefile_dest="${modulefile_dir}/${version}"
     esc='$' envsubst < modulefile.template > "${modulefile_dest}"
     echo "Wrote modulefile to ${modulefile_dest}"
