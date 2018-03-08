76857cad810e04a750f257493574fff981870ca5
Damien Ayers <damien@omad.net>
2018-03-08 15:18:05 +1100

    Add logging to environment build

--- modules/build_environment_module.py
+++ modules/build_environment_module.py
@@ -9,14 +9,16 @@ import urllib.request
 from pathlib import Path
 import shutil
 import string
-
+import logging
 import yaml
 
 MODULE_DIR = '/g/data/v10/public/modules'
 MINICONDA_URL = 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh'
 
+LOG = logging.getLogger('environment_module_builder')
 
 def pre_check(config):
+    LOG.debug('Performing pre-check before installing module')
     if "PYTHONPATH" in os.environ:
         raise Exception("The PYTHONPATH environment variable must NOT be set when creating modules.")
 
@@ -30,6 +32,7 @@ def pre_check(config):
 #    "MODULEPATH=/g/data/v10/public/modules/modulefiles:/apps/.mf:/opt/Modules/modulefiles:/apps/Modules/modulefiles:"
 
 def prep(config_path):
+    LOG.debug('Preparing environment variables')
     # Write files as group and world readable
     os.umask(0o22)
     os.chdir(config_path.parent)
@@ -55,11 +58,13 @@ def make_output_dir(base_path) -> Path:
 
 
 def run(cmd: str):
+    LOG.debug('Running command: %s', cmd)
     return subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
 
 
 def install_conda(conda_conf):
     destination_path = Path(conda_conf['dest'])
+    LOG.debug('Installing miniconda to %s', destination_path)
     with tempfile.TemporaryDirectory() as tmpdir:
         miniconda_installer = tmpdir / "miniconda.sh"
         urllib.request.urlretrieve(conda_conf['url'], miniconda_installer)
@@ -74,6 +79,7 @@ def install_conda(conda_conf):
 
 
 def install_conda_packages(conda_bin_path, from_file):
+    LOG.debug('Installing conda packages from %s', from_file)
     # make sure no .local stuff interferes with the install
     os.environ['PYTHONNOUSERSITE'] = "1"
 
@@ -81,6 +87,7 @@ def install_conda_packages(conda_bin_path, from_file):
 
 
 def write_template(template_file, variables, output_file):
+    LOG.debug('Filling template file %s to %s', template_file, output_file)
     template_contents = template_file.read_text()
     template = string.Template(template_contents)
     output_file.write_text(template.substitute(variables))
@@ -97,10 +104,13 @@ def copy_files(copy_tasks, variables):
         src = Path(task['src'])
         dest = Path(task['dest'])
 
+        LOG.debug('Copying %s to %s', src, dest)
         shutil.copy(src, dest)
 
         if 'chmod' in task:
-            dest.chmod(task['chmod'])
+            perms = task['chmod']
+            LOG.debug('Setting %s permissions to %s', dest, perms)
+            dest.chmod(perms)
 
 
 def read_config(path):
@@ -127,6 +137,7 @@ def include_templated_vars(config):
 
 
 def fix_module_permissions(module_path):
+    LOG.debug('Setting module "%s" to read-only', module_path)
     run(f'chmod -R a-w "${module_path}"')
 
 
@@ -135,10 +146,13 @@ def install_pip_packages(pip_conf, variables):
     pip = pip_conf['pip_cmd']
     dest = pip_conf['dest']
     requirements = pip_conf['requirements']
+    LOG.debug('Installing pip packages from "%s" into directory "%s"',
+              requirements, dest)
     run(f'{pip} install --no-deps --target {dest} --requirement {requirements}')
 
 
 def main(config_path):
+    LOG.debug('Reading config file')
     config = read_config(config_path)
     config['variables']['module_version'] = date()
     include_templated_vars(config)
@@ -161,5 +175,5 @@ def main(config_path):
     copy_and_fill_templates(config.get('template_files', []), variables)
     fix_module_permissions(variables['module_path'])
 
-    if __name__ == '__main__':
-        main(Path(sys.argv[1]))
+if __name__ == '__main__':
+    main(Path(sys.argv[1]))
