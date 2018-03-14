f8b0aa28c4733e512a83bd9e461b31414e216d99
Damien Ayers <damien@omad.net>
2018-03-14 11:12:53 +1100

    Fix linting error in module testing shell script

--- modules/test-module.sh
+++ modules/test-module.sh
@@ -4,7 +4,7 @@
 # Module to test
 module_to_test="$1"
 
-echo Testing module: $module_to_test
+echo Testing module: "$module_to_test"
 
 # Reset modules directory
 export MODULEPATH=/apps/.mf:/opt/Modules/modulefiles:/apps/Modules/modulefiles:
@@ -14,7 +14,7 @@ export MODULEPATH=/apps/.mf:/opt/Modules/modulefiles:/apps/Modules/modulefiles:
 module use /g/data/v10/public/modules/modulefiles
 
 # Load the DEA modules
-module load $module_to_test
+module load "$module_to_test"
 
 
 # TODO Check the output from:
