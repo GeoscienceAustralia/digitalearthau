ae115de4c3545e4ca60de8048734b152fcd67601
Damien Ayers <damien@omad.net>
2018-03-27 11:31:25 +1100

    Explain purpose of requirements_met pbs submission script

--- integration_tests/submit_requirements_met.sh
+++ integration_tests/submit_requirements_met.sh
@@ -4,11 +4,18 @@
 #PBS -l walltime=20:00:00
 #PBS -l mem=20GB
 #PBS -l jobfs=1GB
-#PBS -l ncpus=2
+#PBS -l ncpus=1
 #PBS -e LOGS
 #PBS -o LOGS
 ## The job will be executed from current working directory instead of home.
 #PBS -l wd
+
+# Convert the requirements_met Jupyter notebook to a static HTML version
+# using a PBS job. This is one of the tests we run to verify a new environment
+# module deployment to the NCI is safe to use.
+#
+# The static notebook will be output into the current directory and the logs from
+# PBS will be in the LOGS/ subdirectory.
 
 set -eux
 mkdir -p LOGS
