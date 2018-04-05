053152abadb4691ed06a1faec042f0c8f6a94b63
Damien Ayers <damien@omad.net>
2018-04-05 14:42:50 +1000

    Skip CMI test if CMI server is down

--- digitalearthau/harvest/test_iso19115.py
+++ digitalearthau/harvest/test_iso19115.py
@@ -18,6 +18,16 @@ def sample_iso19115_doc():
     return os.path.join(os.path.split(os.path.realpath(__file__))[0], 'test_iso19115_doc.xml')
 
 
+@pytest.fixture
+def sample_cmi_doc():
+    """Download an ISO19115 document from the live CMI server. (Skip tests if CMI is down)"""
+    try:
+        url = 'http://cmi.ga.gov.au/ecat/115'
+        return iso19115.open_iso_tree(url)
+    except OSError:
+        pytest.skip("CMI Unavailable")
+
+
 def test_harvest_load_mapping_table(mapping_yaml):
     mapping_table = iso19115.load_mapping_table(mapping_yaml)
 
@@ -34,12 +44,9 @@ def test_harvest_attrs(sample_iso19115_doc, mapping_yaml):
     assert global_attrs['license'] == 'CC BY Attribution 4.0 International License'
 
 
-def test_harvest_from_cmi_xml(mapping_yaml):
-    url = 'http://cmi.ga.gov.au/ecat/115'
-
+def test_harvest_from_cmi_xml(mapping_yaml, sample_cmi_doc):
     mapping_table = iso19115.load_mapping_table(mapping_yaml)
-    tree = iso19115.open_iso_tree(url)
-    global_attrs = iso19115.find_attrs_in_tree(tree, mapping_table)
+    global_attrs = iso19115.find_attrs_in_tree(sample_cmi_doc, mapping_table)
 
     assert 'title' in global_attrs
     assert 'uuid' in global_attrs
