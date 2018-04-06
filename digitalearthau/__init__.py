7c02c1be042ac763c8e92e47172e7149792160ef
Damien Ayers <damien@omad.net>
2018-04-06 16:17:38 +1000

    Fix how digitalearthau.MODULE_NAME is defined

--- digitalearthau/__init__.py
+++ digitalearthau/__init__.py
@@ -1,12 +1,18 @@
 from pathlib import Path
+import re
 
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
 
-# TODO: Distinguish between instances: write this var during module build?
-MODULE_NAME = 'dea-prod/{}'.format(__version__)
+match = re.search(r'modules/(.*)/lib', __file__).group(1)
+if match is not None:
+    MODULE_NAME = match.group(1)
+else:
+    MODULE_NAME = 'not_installed_in_a_module'
+del match
+
 
 BASE_DIR = Path(__file__).absolute().parent
 SCRIPT_DIR = BASE_DIR
