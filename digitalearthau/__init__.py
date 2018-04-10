048eb34772751764d39df3988569b00a86a5429b
Damien Ayers <damien@omad.net>
2018-04-10 16:54:50 +1000

    Be slightly less dodgy in __init__.py

--- digitalearthau/__init__.py
+++ digitalearthau/__init__.py
@@ -6,13 +6,16 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-match = re.search(r'modules/(.*)/lib', __file__)
-if match is not None:
-    MODULE_NAME = match.group(1)
-else:
-    MODULE_NAME = 'not_installed_in_a_module'
-del match
 
+def _get_module_name():
+    _match = re.search(r'modules/(.*)/lib', __file__)
+    if _match is not None:
+        return _match.group(1)
+    else:
+        return 'not_installed_in_a_module'
+
+
+MODULE_NAME = _get_module_name()
 
 BASE_DIR = Path(__file__).absolute().parent
 SCRIPT_DIR = BASE_DIR
