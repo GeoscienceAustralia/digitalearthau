7d0c3767651365b42a9b24e7e8bc0e3dcc9031ee
Duncan Gray <duncan.gray@ga.gov.au>
2018-10-19 17:27:37 +1100

    add working test

--- scripts/test_index_nci_modis_lpdaac.py
+++ scripts/test_index_nci_modis_lpdaac.py
@@ -1,17 +1,19 @@
 import unittest
+from pathlib import Path
 from mock import Mock
+from datetime import datetime
 
 from index_nci_modis_lpdaac import *
 
 class Testlpdaac(unittest.TestCase):
 
     def test_modis_path_to_date_range(self):
-        name = "/2017/12.11/MYD13Q1.A2017345.h29v12.006.2017361232038.hdf"
-        file_path = Mock() # 2017/12/12
-        print(file_path.name)
-        modis_path_to_date_range(file_path)
+        name = "/2017/12.11/MYD13Q1.A2017345.h29v12.006.2017361232038.hdf" # not a real file though
+        file_path = Path(name)
+        start_time, end_time = modis_path_to_date_range(file_path)
 
-        self.assertEqual(35, 35)
+        self.assertEqual(start_time, datetime(2017, 12, 11, 0, 0) )
+        self.assertEqual(end_time, datetime(2017, 12, 26, 23, 59, 59, 999999))
 # -------------------------------------------------------------
 if __name__ == "__main__":
     Suite = unittest.makeSuite(Testlpdaac, 'test')
