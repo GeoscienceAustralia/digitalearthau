8d143f52de4557dbba239aed1effbdea8ce21980
Duncan Gray <duncan.gray@ga.gov.au>
2018-11-13 16:12:39 +1100

    Add Comments.

--- scripts/index_modis_lpdaac.sh
+++ scripts/index_modis_lpdaac.sh
@@ -1,7 +1,7 @@
 
 # The argument is the config file location
-./index_nci_modis_lpdaac.py --config $1 index-data /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/2002.07.04
+#./index_nci_modis_lpdaac.py --config $1 index-data /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/2002.07.04
 
-#for i in /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/*; do
-#    ./index_nci_modis_lpdaac.py --config modis_lpdacc.conf index-data $i
-#done
+for i in /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/*; do
+    ./index_nci_modis_lpdaac.py --config modis_lpdacc.conf index-data $i
+done
