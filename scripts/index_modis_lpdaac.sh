4d355017a83e8e812cf8985ae7b9623d3a382b4d
Duncan Gray <duncan.gray@ga.gov.au>
2018-11-20 12:25:30 +1100

    following shellcheck advice cont

--- scripts/index_modis_lpdaac.sh
+++ scripts/index_modis_lpdaac.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 # The argument is the config file location
 #./index_nci_modis_lpdaac.py --config $1 index-data /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/2002.07.04
 
