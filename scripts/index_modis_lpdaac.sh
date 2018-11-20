8c40690d171ed1d972ca66a7f01f0e0931971a6a
Duncan Gray <duncan.gray@ga.gov.au>
2018-11-20 12:16:49 +1100

    following shellcheck advice

--- scripts/index_modis_lpdaac.sh
+++ scripts/index_modis_lpdaac.sh
@@ -3,5 +3,5 @@
 #./index_nci_modis_lpdaac.py --config $1 index-data /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/2002.07.04
 
 for i in /g/data2/u39/public/data/modis/lpdaac-tiles-c6/MYD13Q1.006/*; do
-    ./index_nci_modis_lpdaac.py --config $1 index-data $i
+    ./index_nci_modis_lpdaac.py --config "$1" index-data "$i"
 done
