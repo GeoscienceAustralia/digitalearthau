51ce830088fea152f4691058325351901743558f
Duncan Gray <duncan.gray@ga.gov.au>
2018-12-07 09:04:39 +1100

    comments fix

--- scripts/index_modis_oc.sh
+++ scripts/index_modis_oc.sh
@@ -5,7 +5,7 @@
 # index_modis_oc.sh ../../../modis_oc.conf
 #
 # Before running this script do a create-product;
-# ./index_nci_modis_oc.py create-product --config ../../../modis_oc.conf [PATH]
+# ./index_nci_modis_oc.py --config ../../../modis_oc.conf [PATH] create-product
 # e.g. [PATH]= /g/data2/u39/public/data/modis/oc-1d-aust.v201508.recent/2016/12
 
 for j in /g/data2/u39/public/data/modis/oc-1d-aust.v201508.recent/*; do
