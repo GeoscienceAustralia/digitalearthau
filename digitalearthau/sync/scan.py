6c59d271a936d2b735ffa86c982809a4b29285c1
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-02-15 16:38:38 +1100

    Keep the cache timeout to be 23 hours

--- digitalearthau/sync/scan.py
+++ digitalearthau/sync/scan.py
@@ -25,8 +25,8 @@ from .differences import ArchivedDatasetOnDisk, Mismatch, LocationMissingOnDisk,
 
 _LOG = structlog.get_logger()
 
-# 24 hours (roughly the same day)
-CACHE_TIMEOUT_SECS = 60 * 60 * 24
+# 23 hours (roughly the same day)
+CACHE_TIMEOUT_SECS = 60 * 60 * 23
 
 
 def cache_is_too_old(path):
