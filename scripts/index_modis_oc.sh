ae7b5e12453c36ce303dddb0c31e9e94a79efb76
Damien Ayers <damien@omad.net>
2019-02-19 09:15:00 +1100

    Appease shellcheck god

--- scripts/index_modis_oc.sh
+++ scripts/index_modis_oc.sh
@@ -9,8 +9,8 @@
 # e.g. [PATH]= /g/data2/u39/public/data/modis/oc-1d-aust.v201508.recent/2016/12
 
 for j in /g/data2/u39/public/data/modis/oc-1d-aust.v201508.recent/*; do
-    for i in $j/*; do
-        if [ "$i" != "/g/data2/u39/public/data/modis/oc-1d-aust.v201508.recent/2018/01" ]; then
+    for i in "$j"/*; do
+        if [[ "$i" != "/g/data2/u39/public/data/modis/oc-1d&&ust.v201508.recent/2018/01" ]]; then
             echo "$i"
             ./index_nci_modis_oc.py --config "$1" index-data "$i"
         fi
@@ -19,7 +19,7 @@ done
 
 
 for j in /g/data2/u39/public/data/modis/oc-1d-aust.v201508.past/*; do
-    for i in $j/*; do
+    for i in "$j"/*; do
         ./index_nci_modis_oc.py --config "$1" index-data "$i"
     done
 done
