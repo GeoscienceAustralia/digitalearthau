271aef1b2e39d0578ef992513679056b8d3ffadd
David Gavin <david.gavin@ga.gov.au>
2019-03-14 08:11:01 +1100

    Archived must be NULL

--- scripts/find-new-files/find_new_files.sql
+++ scripts/find-new-files/find_new_files.sql
@@ -2,5 +2,6 @@ select l.uri_body from agdc.dataset_location as l, agdc.dataset as d, agdc.datas
 where l.dataset_ref = d.id
 and  d.dataset_type_ref = t.id
 and t.name = :product_name
+and l.archived is NULL
+and d.archived is NULL
 and l.added between :from_date::timestamp and :to_date::timestamp;
-
