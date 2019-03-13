b9e0a6405121654ace1d6185e8fd76a6d28cd521
David Gavin <david.gavin@ga.gov.au>
2019-03-14 08:00:07 +1100

    Add new file finder script

---
+++ scripts/find-new-files/find_new_files.sql
@@ -0,0 +1,6 @@
+select l.uri_body from agdc.dataset_location as l, agdc.dataset as d, agdc.dataset_type as t
+where l.dataset_ref = d.id
+and  d.dataset_type_ref = t.id
+and t.name = :product_name
+and l.added between :from_date::timestamp and :to_date::timestamp;
+
