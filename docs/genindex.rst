0fee4f023c6d18729830d048162146e09eb52b1f
Damien Ayers <damien@omad.net>
2019-08-30 16:31:41 +1000

    Remove the docs, point to new location

--- docs/genindex.rst
+++
@@ -1,5 +0,0 @@
-Tags Index
-==========
-
-This is a placeholder to use the generated index in the Table of Contents.
-Only the heading will be used.
--- docs/resources/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir ~/dea_examples
-cp -R /g/data/v10/public/digitalearthau/dea_examples/* ~/dea_examples/
-cp /g/data/v10/public/digitalearthau/dea.desktop ~/Desktop/
-
-
-
