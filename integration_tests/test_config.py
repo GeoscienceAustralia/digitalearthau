40650497db9228e36b3a323b584ef667ece2e0b6
Jeremy Hooke <jez@stulk.com>
2020-07-23 10:24:59 +1000

    Add ga l1 products to expected products

--- integration_tests/test_config.py
+++ integration_tests/test_config.py
@@ -109,6 +109,9 @@ def test_dea_config(dea_index: Index):
         'ga_ls5t_ard_3',
         'ga_ls7e_ard_3',
         'ga_ls8c_ard_3',
+        'ga_ls5t_level1_3',
+        'ga_ls7e_level1_3',
+        'ga_ls8c_level1_3',
     }
 
 
