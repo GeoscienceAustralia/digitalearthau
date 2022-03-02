fbdc2efc86446d1c53759cf521d4deffebe6dc75
Damien Ayers <damien@omad.net>
2022-03-03 10:46:23 +1100

    Update all redirects to point to DEA Docs Homepage
    This was originally the documentation home page for digital earth
    australia, hosted at geoscienceaustralia.github.io. We moved it to
    docs.dea.ga.gov.au back in 2019 or so and I changed all the HTML files
    here to redirect to their new location.
    
    However, I made a mistake and files with spaces in them weren't handled correctly.
    
    As well, since then, the structure of the documentation has changed and
    95% of the redirects broke.
    
    Google is currently finding one of the broken redirect pages as the
    first result for "dc.find_datasets".
    
    This should automatically redirect someone who does end up at one of
    these pages, as well as let google know that all pages have permanently
    moved.

--- make-redirects.sh
+++ make-redirects.sh
@@ -1,13 +1,19 @@
-
+#!/bin/bash
 
 find . -name '*.html' -print0 | while read -d $'\0' name
 do
     path="${name#./}"
     cat > "$name" <<EOF
 <!DOCTYPE html>
+<head>
 <meta charset="utf-8">
-<title>Redirecting to https://docs.dea.ga.gov.au/$path</title>
-<meta http-equiv="refresh" content="0; URL=https://docs.dea.ga.gov.au/$path">
-<link rel="canonical" href="https://docs.dea.ga.gov.au/$path">
+<title>Redirecting to https://docs.dea.ga.gov.au/</title>
+<meta http-equiv="refresh" content="0; URL=https://docs.dea.ga.gov.au/">
+</head>
+<body>
+ Digital Earth Australia documentation has 
+ moved to <a href="https://docs.dea.ga.gov.au/">https://docs.dea.ga.gov.au/</a>
+</body>
+</html>
 EOF
 done
