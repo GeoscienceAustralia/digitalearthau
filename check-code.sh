893c1b36dd8d5513a3fe588b28bf32aec6d8b065
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-05-17 05:49:44 +0000

    fix ci

--- check-code.sh
+++ check-code.sh
@@ -28,4 +28,4 @@ yamllint ./**/*.yaml
 # Users can specify extra folders (ie. integration_tests) as arguments.
 pytest -r sx --cov digitalearthau --durations=5 digitalearthau scripts/**/*.py "$@"
 
-eo3-validate --warnings-as-errors digitalearthau/config/eo3
+eo3-validate digitalearthau/config/eo3
